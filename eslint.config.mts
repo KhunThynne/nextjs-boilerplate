080615630ce8c0333d25b15df624f4384419302f
Khunthynne <khunthynne@gmail.com>
2026-04-03 20:08:16 +0700

    chore: update dependencies, generate payload types, add env configuration, and adjust eslint rules

--- eslint.config.mts
+++ eslint.config.mts
@@ -2,9 +2,9 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import ts from "typescript-eslint";
-import js from "@eslint/js";
+
 const eslintConfig = defineConfig([
-  js.configs.recommended,
+  // js.configs.recommended,
   ...ts.configs.recommended,
   ...nextVitals,
   ...nextTs,
--- src/env.ts
+++ src/env.ts
@@ -4,18 +4,18 @@ import { z } from "zod";
 export const env = createEnv({
   server: { PAYLOAD_SECRET: z.string(), POSTGRES_URL: z.string() },
   client: {
-    NEXT_PUBLIC_NODE_ENV: z.enum([
-      "development",
-      "production",
-      "test",
-      "vercel",
-    ]),
+    // NEXT_PUBLIC_NODE_ENV: z.enum([
+    //   "development",
+    //   "production",
+    //   "test",
+    //   "vercel",
+    // ]),
   },
   runtimeEnv: {
     // Server ,
     PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
     POSTGRES_URL: process.env.POSTGRES_URL,
     // Client
-    NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
+    // NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
   },
 });
